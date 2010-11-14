ab8a30e6c1f804fd171bb125533f3dd6fcc2abb2
phoet <phoetmail@googlemail.com>
2010-11-14 13:29:07 +0100

    simplify

--- spec/shootout/bank_code_spec.rb
+++ spec/shootout/bank_code_spec.rb
@@ -4,12 +4,9 @@ describe "bank_code" do
   before { @bank_code, @zip_code = "24050110", "21309" }
 
   [Shootout::SavonBankCode, Shootout::SavonBankCode, Shootout::HandsoapBankCode].each do |clazz|
-    describe "#{clazz} for zip" do
-      it "should return the corrent zip code for a given bank for #{clazz}" do
-        zip_code = clazz.zip_code @bank_code
-        zip_code.should eql @zip_code
-      end
+    it "should return the corrent zip code for a given bank for #{clazz}" do
+      zip_code = clazz.zip_code @bank_code
+      zip_code.should eql @zip_code
     end
   end
-
 end
--- spec/shootout/iban_spec.rb
+++ spec/shootout/iban_spec.rb
@@ -4,12 +4,9 @@ describe "iban" do
   before { @iban = "DE47200505501280133503" }
 
   [Shootout::Soap4rIBAN, Shootout::SavonIBAN, Shootout::HandsoapIBAN].each do |clazz|
-    describe "#{clazz} for iban" do
-      it "should return the validation result for an IBAN for class #{clazz}" do
-        validation_result = clazz.validate @iban
-        validation_result.should eql "VALID" unless validation_result == "Daily Free Usage Limit Exceeded"
-      end
+    it "should return the validation result for an IBAN for class #{clazz}" do
+      validation_result = clazz.validate @iban
+      validation_result.should eql "VALID" unless validation_result == "Daily Free Usage Limit Exceeded"
     end
   end
-
 end
